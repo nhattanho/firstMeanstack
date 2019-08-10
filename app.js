3ebae57f49b71cbdf9f7c50c9aa93ec694b628d9
Nhat Ho <honhattan2411@gmail.com>
2019-08-10 11:29:52 -0700

    Test http module & create a simple server

--- app.js
+++ app.js
@@ -15,11 +15,18 @@ logger.log('honhattan');
 */
 
 const http = require('http');
-const server = http.createServer();
+const server = http.createServer((req, res)=>
+{
+	if(req.url === '/'){
+		res.write('Hello word');
+		res.end();
+	}
+	if(req.url === '/api/courses')
+	{
+		res.write(JSON.stringify([1, 2, 3]));
+		res.end();
+	}
 
-// Register a event 
-server.on('connection', (socket) => {
-	console.log('New connection...');
 });
 
 server.listen(3000); //waiting a raising event on port 3000
