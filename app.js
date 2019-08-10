5de570399e1b6f1f9a02a621b5f923a53f8dc999
Nhat Ho <honhattan2411@gmail.com>
2019-08-10 11:02:35 -0700

    Test http module at low level

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // + Raise an event with arguments
 //event.emit('mesageLog', 1, 'url');
 //event.emit('messageLog', {id: 1, url: 'http://'});
-
+/*
 const Logger = require('./logger'); // import class Logger from module logger.js
 const logger = new Logger();
 
@@ -12,3 +12,16 @@ logger.on('messageLog', (arg)=> {
 });
 
 logger.log('honhattan');
+*/
+
+const http = require('http');
+const server = http.createServer();
+
+// Register a event 
+server.on('connection', (socket) => {
+	console.log('New connection...');
+});
+
+server.listen(3000); //waiting a raising event on port 3000
+
+console.log('Listening on port 3000...');
