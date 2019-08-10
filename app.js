c700d343925ea0bda5dfb382323e13c8820ab329
Nhat Ho <honhattan2411@gmail.com>
2019-08-10 00:15:50 -0700

    Test events module

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Commit: Test export and path module
 var object_logger = require('./logger');
 console.log(object_logger);
 console.log(module);
@@ -5,5 +6,15 @@ console.log(module);
 const path = require('path');
 console.log(path.parse(__filename));
 
+// Commit: Test events module
+const EventEmitter = require('events');
+const event = new EventEmitter();
 
+// + Register a listener for an event
+event.on('messageLog', function(){
+	console.log('Listener called');
+});
+
+// + Raise an event
+event.emit('messageLog');
 
