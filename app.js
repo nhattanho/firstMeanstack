deeec978f1f8a340aac87a444f0a8520e776e704
Nhat Ho <honhattan2411@gmail.com>
2019-08-10 00:50:49 -0700

    Test events module with arguments

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 // Commit: Test export and path module
 var object_logger = require('./logger');
 console.log(object_logger);
@@ -5,16 +6,18 @@ console.log(module);
 
 const path = require('path');
 console.log(path.parse(__filename));
+*/
 
 // Commit: Test events module
 const EventEmitter = require('events');
 const event = new EventEmitter();
 
-// + Register a listener for an event
-event.on('messageLog', function(){
-	console.log('Listener called');
+// + Register a listener for an event with arguments
+event.on('messageLog', function(eventArgs){
+	console.log('Listener called', eventArgs);
 });
 
-// + Raise an event
-event.emit('messageLog');
+// + Raise an event with arguments
+//event.emit('mesageLog', 1, 'url');
+event.emit('messageLog', {id: 1, url: 'http://'});
 
