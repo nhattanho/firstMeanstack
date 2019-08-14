a63569a32e8b833e040e886c3de5a135cc4491fc
Nhat Ho <honhattan2411@gmail.com>
2019-08-14 12:03:10 -0700

    Testing a simple restful API with GET POST PUT DELETE

--- app.js
+++ app.js
@@ -13,7 +13,12 @@ logger.on('messageLog', (arg)=> {
 
 logger.log('honhattan');
 */
+const Joi = require('joi');
+const express = require('express');
+const app = express();
 
+app.use(express.json());
+/*
 const http = require('http');
 const server = http.createServer((req, res)=>
 {
@@ -32,8 +37,116 @@ const server = http.createServer((req, res)=>
 server.listen(3000); //waiting a raising event on port 3000
 
 console.log('Listening on port 3000...');
+*/
 
-const add_test = require('add_public');
+const courses = [
+	{ id: 1, name: 'course1' },
+	{ id: 2, name: 'course2' },
+	{ id: 3, name: 'course3' }
+];
 
-console.log(add_test.add(3,4));
-console.log(add_test.multiply(2,4));
+app.get('/', (req,res)=>{
+	res.send('hello word');
+});
+
+app.get('/api/list', (req, res)=>{
+	res.send((courses));
+});
+
+//1 parameter
+app.get('/api/courses/:id', (req, res)=>{
+	//courses.find(c => c.id === parseInt(req.params.id));
+	const course = courses.find(function(element){
+		return element.id === parseInt(req.params.id);
+	});
+	if(!course) return res.status(404).send('The course with given ID was not found');
+	res.send(course);
+	console.log(course);
+});
+
+//2 parameters
+app.get('/api/posts/:year/:month', (req, res)=>{
+        res.send(req.params);
+});
+
+//Create an object
+app.post('/api/courses', (req, res)=>{
+
+	/*
+	const schema = {
+		name: Joi.string().min(3).required()
+	};
+
+	const result = Joi.validate(req.body, schema);
+	if(result.error){
+		res.status(400).send(result.error.details[0].message);
+		return;
+	}
+	*/
+	
+	const {error} = validateCourse(req.body);
+	if(error) return res.status(400).send(error.details[0].message);
+
+	const course = {
+		id: courses.length + 1,
+		name: req.body.name
+	};
+	courses.push(course);
+	//res.send(course);
+	res.send(courses);
+});
+
+//Update the object
+app.put('/api/courses/:id', (req,res)=>{
+	//Look up the course, if not existing -> return 404
+	const course = courses.find(element => element.id === parseInt(req.params.id));
+	if(!course) return res.status(404).send('The course with given ID was not found');
+	
+	/*
+	//Validate, if invalid, return 400 - bad request
+	const schema = {
+		name: Joi.string().min(3).required()
+	};
+	const result = Joi.validate(req.body, schema);
+	if(result.error){
+		res.status(400).send(result.error.details[0].message);
+                return;
+        }
+	*/
+	
+	// { error }: the way to get the property of result of validate function
+	const {error} = validateCourse(req.body);// const result = ... =>result.error...
+	if(error) return res.status(400).send(error.details[0].message);
+
+	//Update course
+	course.name = req.body.name;
+	//Return the updated course
+	res.send(course);
+});
+
+function validateCourse(course)
+{
+	const schema = {
+		name: Joi.string().min(3).required() 
+	};
+	return Joi.validate(course, schema);
+}	
+
+app.delete('/api/courses/:id', (req, res)=>{
+	//Look up the course
+	const course = courses.find(element => element.id === parseInt(req.params.id));
+	if(!course) return res.status(404).send('The course with given ID was not found');
+
+	const index = courses.indexOf(course);
+	courses.splice(index,1);
+
+	//res.send(course);
+	res.send(courses);
+});
+
+//PORT
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+        console.log(`listening on port ${port}...`);
+});
+
