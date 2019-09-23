945444182438c586ac8e2e0dbb23f72f65535042
Nhat Ho <honhattan2411@gmail.com>
2019-09-23 04:27:31 -0700

    Callback function hell

--- async_test/index.js
+++ async_test/index.js
@@ -1,10 +1,23 @@
 console.log('Before');
 
+//Asynchronous ==> nested ==> Callback Hell problem
 const user = getUser(1, user=>{
 	getRepo(user.gitHubUsername, (respons)=>{
-		console.log("Response", respons);
+		getCommits(respons.repo1, (commits)=> {
+			console.log('Commits are: ' + commits);
+		});
 	});
 });
+
+/*
+//Synchronous
+console.logog('Before');
+const user = getUser(1);
+const repos = getRepo(user.gitHubUsername);
+const commits = getCommits(reppos[0]);
+console.logog('After');
+*/
+
 
 console.log('After');
 
@@ -20,4 +33,10 @@ function getRepo(id, callback){
 		console.log('Calling GitHub API....');
 		callback (['repo1', 'repo2', 'repo3']);
 	});
+}
+
+function getCommits(repo, callback)
+{
+	console.log('Get commits...');
+	callback(["This commit for callback hell function in asynchronous!"]);
 }
