448835fba2eb0386352c126e5218b59143edd5aa
Nhat Ho <honhattan2411@gmail.com>
2019-09-29 07:08:49 -0700

    Avoiding case of callback hell function by using specific functions

--- async_test/index.js
+++ async_test/index.js
@@ -1,42 +1,51 @@
 console.log('Before');
 
 //Asynchronous ==> nested ==> Callback Hell problem
-const user = getUser(1, user=>{
-	getRepo(user.gitHubUsername, (respons)=>{
-		getCommits(respons.repo1, (commits)=> {
-			console.log('Commits are: ' + commits);
-		});
-	});
-});
+const user = getUser(1, getRepo1);
 
-/*
-//Synchronous
-console.logog('Before');
-const user = getUser(1);
-const repos = getRepo(user.gitHubUsername);
-const commits = getCommits(reppos[0]);
-console.logog('After');
-*/
-
-
-console.log('After');
-
+//Replace callback function by specific functions
+//In fact, creating timeout to simulation a real case and proving the asynchronous of nodejs 
 function getUser(id, callback) {
+	console.log('In getUser function');
 	setTimeout(()=>{
 		console.log('Reading a user from a database..');
 		callback({id: id, gitHubUsername: 'Nhat Ho'});
 	},2000);
 }
 
+function getRepo1(user)
+{
+	console.log('In getRepo1 function');
+	console.log('user id: ', user.id);
+	getRepo(user.id, (getCommits1));
+}
+
 function getRepo(id, callback){
+	console.log('In getRepo2 function.')
 	setTimeout( ()=> {
 		console.log('Calling GitHub API....');
-		callback (['repo1', 'repo2', 'repo3']);
+		callback({repo1: 'repo1', repo2: 'repo2', repo3: 'repo3'});
 	});
+}
+
+function getCommits1(respons)
+{
+	console.log('In getCommits1');
+	console.log('respons name:', respons.repo1);
+	getCommits(respons.repo1, displayCommits);
 }
 
 function getCommits(repo, callback)
 {
+	console.log('In function getCommits and value of repo is', repo);
 	console.log('Get commits...');
 	callback(["This commit for callback hell function in asynchronous!"]);
 }
+
+function displayCommits(commits)
+{
+	console.log('Commits are: ' + commits);
+}
+
+//=====================================================
+console.log('After');
